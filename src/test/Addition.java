8255c00801c4b619dd196e1eeccc01e2899fcd10
nickaggelidis26 <nickaggelidis26@gmail.com>
2025-05-18 17:40:10 +0300

    feat: addition class

--- src/test/Addition.java
+++ src/test/Addition.java
@@ -1,4 +1,13 @@
 package test;
 
 public class Addition {
+
+    public static void main(String[] args) {
+
+        System.out.println(add(3,5));
+    }
+
+    public static int add (int a , int b){
+        return  a + b;
+    }
 }
